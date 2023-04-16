01bfd06df128e9dc95da51a0f321691a0b3fa7c4
mikhail <mikhail@Ubuntu.myguest.virtualbox.org>
2023-04-16 22:37:04 +0300

    Add welcome-function and import readline-sync lib

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,4 @@
-console.log('Welcome to the Brain Games!')
+#!/usr/bin/env node
+import welcome from '../src/cli.js';
+
+welcome();
---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const welcome = () => {
+ console.log('Welcome to the Brain Games!')
+ const username = readlineSync.question('May I have your name? ');
+ console.log('Hello ' + username + '!');
+};
+export default welcome;
+
