8ae9efd97a321dd88ecf59e35339406ed81aae04
mikhail <mikhail@Ubuntu.myguest.virtualbox.org>
2023-04-17 01:26:48 +0300

    Add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const welcome = () => {
- console.log('Welcome to the Brain Games!')
- const username = readlineSync.question('May I have your name? ');
- console.log('Hello ' + username + '!');
+  console.log('Welcome to the Brain Games!');
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${username}!`);
 };
 export default welcome;
-
