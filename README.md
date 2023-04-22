0d168142cc82dfa8036fd62db79f5382a727fbdf
mikhail <mikhail@Ubuntu.myguest.virtualbox.org>
2023-04-22 14:02:46 +0300

    Add brain-even and asciinema

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/xG2x/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/xG2x/frontend-project-44/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/17aa0613e510ac0eda5f/maintainability)](https://codeclimate.com/github/xG2x/frontend-project-44/maintainability)
+
+<a href="https://asciinema.org/a/RjUxQACpPV1XBqZSow0pNgXfw">
---
+++ src/brain-even.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+
+const COUNT = 3; // number of repeating questions;
+
+const getCorrectAnswer = (number) => {
+  const isEven = number % 2 === 0;
+  return isEven ? 'yes' : 'no';
+};
+
+const askQuestion = (number) => {
+  console.log(`Question: ${number}`);
+  return readlineSync.question('Your answer: ');
+};
+
+const brainEven = () => {
+  let count = 1;
+  console.log('Welcome to the Brain Games!');
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${username}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  while (count <= COUNT) {
+    const number = Math.round(Math.random() * 100); // random number in the range from 0 to 100;;
+    const correctAnswer = getCorrectAnswer(number);
+    const answer = askQuestion(number);
+    if (answer === correctAnswer) {
+      console.log('Correct');
+      if (count === COUNT) {
+        console.log(`Congratulations, ${username}!`);
+      }
+      count += 1;
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${username}`);
+      break;
+    }
+  }
+};
+
+export default brainEven;
