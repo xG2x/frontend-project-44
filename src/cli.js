7fe0a5836b54a3bf7697731710673694eb5f5139
xG2x <kuzubek@gmail.com>
2023-04-23 15:06:54 +0300

    Idk why I have errors

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
-  const username = readlineSync.question('May I have your name? ');
+  const username = readlineSync.question('May I have your name? \n');
   console.log(`Hello ${username}!`);
 };
 export default welcome;
