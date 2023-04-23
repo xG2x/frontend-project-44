4eafc9d4c41967ea3229357ceb5da83222a58905
xG2x <kuzubek@gmail.com>
2023-04-23 15:21:01 +0300

    Fix typo

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const gameLogic = (game, description) => {
       }
       count += 1;
     } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}`);
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
       break;
     }
   }
