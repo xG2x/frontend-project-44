ddce7f3f9f6070dcb118bf3af929237fabce43af
xG2x <kuzubek@gmail.com>
2023-04-23 15:11:08 +0300

    Maybe that fix cli.js help

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
-  const username = readlineSync.question('May I have your name? \n');
-  console.log(`Hello ${username}!`);
+  const username = readlineSync.question('May I have your name? ');
+  return console.log(`Hello ${username}!`);
 };
 export default welcome;
