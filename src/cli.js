443395e4c3c51b8ff0af25546fade265ad3f4aa6
xG2x <kuzubek@gmail.com>
2023-04-23 15:14:09 +0300

    Cazzo di comma

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
-  return console.log(`Hello ${username}!`);
+  console.log(`Hello, ${username}!`);
 };
 export default welcome;
